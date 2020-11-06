4eff6d2a7c87a02b9f3f3268b05d5173fd616db1
Kyle Galer <chembot81@hotmail.com>
2020-11-05 18:40:58 -0600

    ajax calls

---
+++ script.js
@@ -0,0 +1,31 @@
+$("#city-btn").on("click", function () {
+    event.preventDefault();
+    console.log("clicked");
+    let cityName = $("#city-search").val().trim();
+    let currentURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  cityName +
+  "&appid=ab71125de5aff9fb594cb0a9e4c3dade";
+    let fiveDayURL="http://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName + "&appid=ab71125de5aff9fb594cb0a9e4c3dade";
+   // let UVIndexURL="http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=ab71125de5aff9fb594cb0a9e4c3dade"
+  
+  $.ajax({
+    url: currentURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    
+    let humidity = response.main.humidity;
+    let humidityP = $("<p>")
+    humidityP.text(humidity)
+    $("#humidity").append(humidityP);
+  });
+
+  $.ajax({
+    url: fiveDayURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+});
